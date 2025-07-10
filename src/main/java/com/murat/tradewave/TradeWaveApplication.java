e4041e56d7b7ff4cc3acce749fdc5cd13d9f35ac
muratguven123 <muratguven376@gmail.com>
2025-07-10 22:16:35 +0300

    Initial TradeWawe project setup

---
+++ src/main/java/com/murat/tradewave/TradeWaveApplication.java
@@ -0,0 +1,13 @@
+package com.murat.tradewave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeWaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeWaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/murat/tradewave/TradeWaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murat.tradewave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeWaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
