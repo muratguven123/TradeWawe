5224194ea042640ec14a84d1579cddef24916051
muratguven123 <muratguven376@gmail.com>
2025-07-31 17:06:20 +0300

    lombok failed is fixed

--- src/main/java/com/murat/tradewave/model/User.java
+++ src/main/java/com/murat/tradewave/model/User.java
@@ -15,58 +15,28 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 public class User {
+    @Setter
+    @Getter
     @Id
     @GeneratedValue
     private long id;
+    @Setter
+    @Getter
     @Column(nullable = false)
     private String email;
+    @Setter
+    @Getter
     @Column(nullable = false)
     private String password;
 
+    @Setter
+    @Getter
     private String name;
 
+    @Setter
+    @Getter
     @Enumerated(EnumType.STRING)
     private Role role;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Address> addresses = new ArrayList<>();
